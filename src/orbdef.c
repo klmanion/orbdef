2132c7a5f1e4f3137c8757e8e6e6ddd4e784dd3a
Kurt L. Manion <klmanion@gmail.com>
2021-08-08 11:39:28 -0400

    introduction of screens

--- src/orbdef.c
+++ src/orbdef.c
@@ -10,19 +10,73 @@
 #include "dir.h"
 #include "entity.h"	/* may remove after cursor is implemented */
 #include "tower.h"
+#include "screen.h"
 
-static	tower_t	*tower_lst[8];
-static	int	 tower_num;
+typedef
+struct Common
+{
+	tower_t	*spire;
+} common_data_t;
+
+screen_t*
+battle_run(
+    screen_t *const	 scr,
+    void		*vptr)
+{
+	screen_data_t data;
+	common_data_t *cmn;
+
+	tower_t *spire;
+
+	chtype ch;
+	screen_t *retv;
+
+	data = (screen_data_t)scr->data;
+	cmn = (common_data_t *)vptr;
+
+	spire = cmn->spire;
+
+	retv = scr;
+
+	for (bool running=true; running; )
+	    {
+		/* drawing */
+		tower_draw(spire);
+
+		refresh();
+
+		/* polling/handling */
+		switch ((ch = getch())) {
+		case KEY_F(1):
+			running = false;
+			retv = (screen_t *)NULL;
+			break;;
+
+		default:
+			break;;
+		}
+	    }
+
+	return retv;
+}
 
 int
 orbdef()
 {
+	screen_t *battle;
+	screen_t *scr;
+	common_data_t cmn;
 	int row,col;
 	tower_t spire;
-	chtype ch;
+//	tower_t	*tower_lst[8];
+//	int	 tower_num;
 
-	memset(tower_lst, (int)NULL, 8);
-	tower_num = 0;
+	screen_init(screen_alloc(&battle),
+		    (screen_data_t)NULL,
+		    battle_run);
+
+//	memset(tower_lst, (int)NULL, 8);
+//	tower_num = 0;
 
 	getmaxyx(stdscr, row,col);
 	tower_init(&spire, row/2,col/2);
@@ -31,92 +85,77 @@ orbdef()
 	tower_shell_add(&spire, shell_init(NULL, 1, 0,0));
 	tower_shell_add(&spire, shell_init(NULL, 2, 0,0));
 	tower_shell_add(&spire, shell_init(NULL, 3, 0,0));
-	tower_draw(&spire);
 
-	refresh();	getch();
-	return 0;
+	cmn.spire = &spire;
 
 	/* place surrounding towers */
-	entity_t cursor;
+//	entity_t cursor;
+//
+//	entity_init(&cursor, row/2,col/2);
+//
+//	for (bool running=true; running; )
+//	    {
+//		clear();
+//
+//		tower_draw(&spire);
+//
+//		for (size_t n=0; n<tower_num; ++n)
+//		    tower_draw(tower_lst[n]);
+//
+//		attron(A_REVERSE);
+//		move(entity_pos_y(&cursor),entity_pos_x(&cursor));
+//		addch(inch() & A_CHARTEXT);
+//		attroff(A_REVERSE);
+//
+//		refresh();
+//
+//		switch ((ch = getch())) {
+//		case KEY_F(1):
+//			running = false;
+//			break;;
+//
+//		/* TODO: may not create tower in spire's radius */
+//		/* TODO: bound checking */
+//		/* TODO: cursor object */
+//		case 'j':
+//		case KEY_DOWN:
+//			entity_mvdir(&cursor, dir_bot, 1);	break;;
+//		case 'k':
+//		case KEY_UP:
+//			entity_mvdir(&cursor, dir_top, 1);	break;;
+//		case 'h':
+//		case KEY_LEFT:
+//			entity_mvdir(&cursor, dir_cl, 1);	break;;
+//		case 'l':
+//		case KEY_RIGHT:
+//			entity_mvdir(&cursor, dir_cr, 1);	break;;
+//		case 'y':
+//			entity_mvdir(&cursor, dir_tl, 1);	break;;
+//		case 'u':
+//			entity_mvdir(&cursor, dir_tr, 1);	break;;
+//		case 'b':
+//			entity_mvdir(&cursor, dir_bl, 1);	break;;
+//		case 'n':
+//			entity_mvdir(&cursor, dir_br, 1);	break;;
+//
+//		case '\r':	/* place tower */
+//			tower_lst[tower_num++] = 
+//				tower_init(NULL,
+//					   entity_pos_y(&cursor),
+//					   entity_pos_x(&cursor));
+//			break;;
+//
+//		default:
+//			break;;
+//		}
+//	    }
 
-	entity_init(&cursor, row/2,col/2);
-
-	for (bool running=true; running; )
-	    {
-		clear();
-
-		tower_draw(&spire);
-
-		for (size_t n=0; n<tower_num; ++n)
-		    tower_draw(tower_lst[n]);
-
-		attron(A_REVERSE);
-		move(entity_pos_y(&cursor),entity_pos_x(&cursor));
-		addch(inch() & A_CHARTEXT);
-		attroff(A_REVERSE);
-
-		refresh();
-
-		switch ((ch = getch())) {
-		case KEY_F(1):
-			running = false;
-			break;;
-
-		/* TODO: may not create tower in spire's radius */
-		/* TODO: bound checking */
-		/* TODO: cursor object */
-		case 'j':
-		case KEY_DOWN:
-			entity_mvdir(&cursor, dir_bot, 1);	break;;
-		case 'k':
-		case KEY_UP:
-			entity_mvdir(&cursor, dir_top, 1);	break;;
-		case 'h':
-		case KEY_LEFT:
-			entity_mvdir(&cursor, dir_cl, 1);	break;;
-		case 'l':
-		case KEY_RIGHT:
-			entity_mvdir(&cursor, dir_cr, 1);	break;;
-		case 'y':
-			entity_mvdir(&cursor, dir_tl, 1);	break;;
-		case 'u':
-			entity_mvdir(&cursor, dir_tr, 1);	break;;
-		case 'b':
-			entity_mvdir(&cursor, dir_bl, 1);	break;;
-		case 'n':
-			entity_mvdir(&cursor, dir_br, 1);	break;;
-
-		case '\r':	/* place tower */
-			tower_lst[tower_num++] = 
-				tower_init(NULL,
-					   entity_pos_y(&cursor),
-					   entity_pos_x(&cursor));
-			break;;
-
-		default:
-			break;;
-		}
-	    }
-#ifdef DEBUG
-	return 1;
-#endif
-
-	for (bool running=true; running; )
-	    {
-		tower_draw(&spire);
-
-		refresh();
-
-		switch ((ch = getch())) {
-		case KEY_F(1):
-			running = false;
-
-		default:
-			break;;
-		}
-	    }
+	scr = battle;
+	while ((scr = screen_run(scr, (void *)&cmn))) { }
 
 	tower_deinit(&spire);
+
+	battle = screen_free(&battle);
 
 	return 1;
 }
---
+++ src/screen.h
@@ -0,0 +1,28 @@
+/** screen.h
+ */
+
+#ifndef _SCREEN_H_
+#define _SCREEN_H_
+
+#include <stdlib.h>
+
+typedef	void *	screen_data_t;
+typedef struct Screen* (*screen_funct_t)(struct Screen *const,void *);
+
+typedef
+struct Screen
+{
+	screen_data_t	data;	/* environment common among screens */
+	screen_funct_t	run;
+} screen_t;
+
+screen_t*	screen_alloc (screen_t **);
+screen_t*	screen_init (screen_t *,screen_data_t,screen_funct_t);
+screen_t*	screen_deinit (screen_t *);
+screen_t*	screen_free (screen_t **);
+
+screen_t*	screen_run (screen_t *const,void *);
+
+#endif /* !_SCREEN_H_ */
+
+/* vi: set ts=8 sw=8 noexpandtab tw=79: */
