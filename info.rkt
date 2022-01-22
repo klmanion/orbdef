61d32c769ac7b8c46d54aca2a035a3f25eedd6ff
klmanion <klmanion@gmail.com>
2022-01-22 12:20:34 -0500

    added racket package info file

---
+++ info.rkt
@@ -0,0 +1,8 @@
+#lang info
+(define collection "orbdef")
+(define deps '("base" "lux"))
+(define build-deps '("scribble-lib" "racket-doc" "rackunit-lib"))
+(define scribblings '(("scribblings/rain.scrbl" ())))
+(define pkg-desc "A rouge-like economy based wargame")
+(define version "0.0")
+(define pkg-authors '(Kurt L. Manion))
